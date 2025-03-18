2bd77f4e3e5d3ef635b2c7e3193b499a9c7557ae
ARINDAM GHOSH <128517088+arindam2590@users.noreply.github.com>
2025-03-18 21:19:42 +0530

    Add files via upload

--- agent.py
+++ agent.py
@@ -19,7 +19,7 @@ class GeneticAgent:
         self.population_size = self.params['POPULATION_SIZE']
         self.mutation_rate = self.params['MUTATION_RATE']
         self.num_generations = self.params['NUM_GENERATIONS']
-        self.gen_algo = GeneticAlgorithm(self.population_size, self.mutation_rate)
+        self.gen_algo = GeneticAlgorithm(self.maze, self.src, self.population_size, self.mutation_rate)
         self.best_fitness = -float('inf')  # Track best fitness across generations
         self.best_fitness_list = []
         self.best_solution_list = []
@@ -41,7 +41,8 @@ class GeneticAgent:
         for generation in range(self.num_generations):
             print(f"Generation {generation + 1}:")
 
-            fitness = [self.evaluate_fitness(individual) for individual in self.population]
+            fitness_scores = [self.evaluate_fitness(individual) for individual in self.population]
+            # print(fitness)
             # filtered_fitness = self.remove_infeasible_individuals(fitness)
             # filtered_population, filtered_fitness = zip(*[(individual, fit) for individual, fit in
             #                                               zip(self.population, fitness) if fit[1]]) \
@@ -50,10 +51,10 @@ class GeneticAgent:
             # self.population_size = len(filtered_population)
             # filtered_fitness = list(filtered_fitness)
 
-            norm_fitness_scores = self.normalized_fitness(fitness, True)
+            # norm_fitness_scores = self.normalized_fitness(fitness, True)
 
-            best_fitness_index = np.argmax(norm_fitness_scores)
-            best_value_generation = norm_fitness_scores[best_fitness_index]
+            best_fitness_index = np.argmax(fitness_scores)
+            best_value_generation = fitness_scores[best_fitness_index]
             best_solution_generation = self.population[best_fitness_index]
             self.best_fitness_list.append(best_value_generation)
             self.best_solution_list.append(best_solution_generation)
@@ -68,7 +69,7 @@ class GeneticAgent:
             else:
                 print(f'Info: Best solution is not improved in this Generation')
 
-            new_population = self.run_generation(norm_fitness_scores)
+            new_population = self.run_generation(fitness_scores)
             self.population = new_population
             print(f'-' * 101)
 
@@ -81,9 +82,9 @@ class GeneticAgent:
         print(f'Info: Size of first Parents - {len(parent1)} and Size of second Parents - {len(parent2)}')
 
         for p1, p2 in zip(parent1, parent2):
-            child1, child2 = self.gen_algo.crossover(p1, p2)
-            new_population.append(self.gen_algo.mutate(child1))
-            new_population.append(self.gen_algo.mutate(child2))
+            child = self.gen_algo.crossover(p1, p2)
+            new_population.append(self.gen_algo.mutate(child))
+            # new_population.append(child)
         print(f'Info: Mutation Rate for the Child - {self.mutation_rate}')
         elite_individuals = [best_ind[0] for best_ind in
                              sorted(zip(self.population, fitness_scores), key=lambda x: x[1],
@@ -95,45 +96,65 @@ class GeneticAgent:
         print(f'Info: Size of the New Population - {self.population_size}')
         return new_population
 
+    # def evaluate_fitness(self, individual):
+    #     x, y = self.src
+    #     individual_len, turn_count, feasibility_score, feasible_flag = 1, 0, 0, True
+    #
+    #     for i in range(len(individual) - 1):
+    #         if individual[i] != individual[i + 1]:
+    #             turn_count += 1
+    #
+    #     for gene in individual:
+    #         individual_len += 1
+    #         x, y = x + (gene == 'D') - (gene == 'U'), y + (gene == 'R') - (gene == 'L')
+    #
+    #         if x < 0 or x >= self.maze.shape[0] or y < 0 or y >= self.maze.shape[1] or self.maze[x][y] == 1:
+    #             feasible_flag = False
+    #             return [-5, feasible_flag, turn_count, individual_len]
+    #
+    #         feasibility_score += 1
+    #
+    #     return [feasibility_score + 5 if (x, y) == tuple(self.dst) else feasibility_score,
+    #             feasible_flag, turn_count, individual_len]
+
     def evaluate_fitness(self, individual):
         x, y = self.src
-        individual_len, turn_count, feasibility_score, feasible_flag = 1, 0, 0, True
-
-        for i in range(len(individual) - 1):
-            if individual[i] != individual[i + 1]:
-                turn_count += 1
-
+        fitness = penalty = dist_fitness = 0.0
         for gene in individual:
-            individual_len += 1
             x, y = x + (gene == 'D') - (gene == 'U'), y + (gene == 'R') - (gene == 'L')
-
             if x < 0 or x >= self.maze.shape[0] or y < 0 or y >= self.maze.shape[1] or self.maze[x][y] == 1:
-                feasible_flag = False
-                return [-5, feasible_flag, turn_count, individual_len]
+                penalty -= 1.0
+            else:
+                if (x, y) == tuple(self.dst):
+                    fitness += 5.0
+                else:
+                    fitness += 1.0
+            dist_fitness = self.euclidean_distance((x, y))
+        fitness = fitness - (penalty + dist_fitness)
+        return fitness
 
-            feasibility_score += 1
+    def euclidean_distance(self, point1):
+        x1, y1 = point1
+        x2, y2 = self.dst
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
-        return [feasibility_score + 5 if (x, y) == tuple(self.dst) else feasibility_score,
-                feasible_flag, turn_count, individual_len]
-
-    def normalized_fitness(self, fitness, flag=False):
-        fitness_array = np.array(fitness, dtype=float)
-        min_vals, max_vals = fitness_array.min(axis=0), fitness_array.max(axis=0)
-
-        # if flag:
-        #     print(f'Info: Min. Feasibility: {min_vals[0]: .3f}, Max. Feasibility: {max_vals[0]: .3f}, '
-        #           f'Min. Turns: {min_vals[1]: .6f}, Max. Turns: {max_vals[1]: .6f}, Min. Length: {min_vals[2]: .3f},'
-        #           f'Max. Length: {max_vals[2]: .3f}')
-
-        norm_fitness = [((f[0] - min_vals[0]) / (max_vals[0] - min_vals[0]) if max_vals[0] != min_vals[0] else 1.0,
-                         1 - (f[1] - min_vals[1]) / (max_vals[1] - min_vals[1]) if max_vals[1] != min_vals[1] else 1.0,
-                         1 - (f[2] - min_vals[2]) / (max_vals[2] - min_vals[2]) if max_vals[2] != min_vals[2] else 1.0)
-                        for f in fitness]
-
-        weighted_norm_fitness = [(self.weighted_feas * score[0]) + (self.weighted_turn * score[1]) +
-                                 (self.weighted_len * score[2]) for score in norm_fitness]
-
-        return weighted_norm_fitness
+    # def normalized_fitness(self, fitness, flag=False):
+    #     fitness_array = np.array(fitness, dtype=float)
+    #     print(fitness_array)
+    #     min_vals, max_vals = fitness_array.min(axis=0), fitness_array.max(axis=0)
+    #
+    #     # if flag:
+    #     #     print(f'Info: Min. Feasibility: {min_vals[0]: .3f}, Max. Feasibility: {max_vals[0]: .3f}, '
+    #     #           f'Min. Turns: {min_vals[1]: .6f}, Max. Turns: {max_vals[1]: .6f}, Min. Length: {min_vals[2]: .3f},'
+    #     #           f'Max. Length: {max_vals[2]: .3f}')
+    #
+    #     # norm_fitness = [(f[0] - min_vals[0]) / (max_vals[0] - min_vals[0]) if max_vals[0] != min_vals[0] else 1.0,
+    #     #                 for f in fitness]
+    #
+    #     # weighted_norm_fitness = [(self.weighted_feas * score[0]) + (self.weighted_turn * score[1]) +
+    #     #                          (self.weighted_len * score[2]) for score in norm_fitness]
+    #
+    #     return 0 #weighted_norm_fitness
 
     def display_maze(self):
         self.running = True
--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -4,10 +4,12 @@ import numpy as np
 
 
 class GeneticAlgorithm:
-    def __init__(self, pop_size, mut_rate):
+    def __init__(self, maze, source, pop_size, mut_rate):
         with open('Utils/config.json', 'r') as file:
             self.params = json.load(file)
 
+        self.maze = maze
+        self.src = source
         self.population_size = pop_size
         self.lower_bound = self.params['CHROMOSOME_LOWER']
         self.upper_bound = self.params['CHROMOSOME_UPPER']
@@ -21,46 +23,115 @@ class GeneticAlgorithm:
         population = []
         for i in range(self.population_size):
             path_length = np.random.randint(self.lower_bound, self.upper_bound)
-            path = ''.join(random.choices(['U', 'D', 'L', 'R'], k=path_length))
-            population.append(path)
+            chromosome = []
+
+            previous_move = random.choice(['U', 'D', 'L', 'R'])
+            chromosome.append(previous_move)
+            for _ in range(1, path_length):
+                next_move = random.choice(['U', 'D', 'L', 'R'])
+                while (previous_move == 'U' and next_move == 'D') or (previous_move == 'D' and next_move == 'U') or \
+                        (previous_move == 'L' and next_move == 'R') or (previous_move == 'R' and next_move == 'L'):
+                    next_move = random.choice(['U', 'D', 'L', 'R'])
+                chromosome.append(next_move)
+                previous_move = next_move
+            population.append(''.join(chromosome))
         return population
 
+    # def select_parents(self, population, fitness_scores):
+    #     self.population_size = len(population)
+    #     self.tournament_size = int(self.tour_size_percent * self.population_size)
+    #     self.n_parents = int(self.parent_percent * self.tournament_size)
+    #
+    #     print(f'Info: Size of the Tournament - {self.tournament_size}')
+    #     parent1, parent2 = [], []
+    #     # Select two parents from the population using tournament selection
+    #     for i in range(self.n_parents):
+    #         parent1.append(self.tournament_selection(population, fitness_scores))
+    #         parent2.append(self.tournament_selection(population, fitness_scores))
+    #     return parent1, parent2
+
     def select_parents(self, population, fitness_scores):
-        self.population_size = len(population)
+        filtered_population, filtered_fitness = zip(*[(individual, fit) for individual, fit in
+                                                      zip(population, fitness_scores) if fit >= 0]) \
+            if any(fit >= 0 for fit in fitness_scores) else ([], [])
+
+        if len(filtered_population) == 0:
+            print(f"Warning: All individuals had negative fitness! Regenerating population...")
+            exit(0)
+
+        sorted_population = [x for _, x in sorted(zip(filtered_fitness, filtered_population), reverse=True)]
+        sorted_fitness = sorted(filtered_fitness, reverse=True)
+
+        self.population_size = len(sorted_population)
         self.tournament_size = int(self.tour_size_percent * self.population_size)
         self.n_parents = int(self.parent_percent * self.tournament_size)
 
         print(f'Info: Size of the Tournament - {self.tournament_size}')
         parent1, parent2 = [], []
-        # Select two parents from the population using tournament selection
         for i in range(self.n_parents):
-            parent1.append(self.tournament_selection(population, fitness_scores))
-            parent2.append(self.tournament_selection(population, fitness_scores))
+            parent1.append(self.tournament_selection(sorted_population, sorted_fitness))
+            parent2.append(self.tournament_selection(sorted_population, sorted_fitness))
         return parent1, parent2
 
     def tournament_selection(self, population, fitness_scores):
         # Randomly select 'tournament_size' individuals (by index) from the population.
         contestant_indices = random.sample(range(self.population_size), self.tournament_size)
-
         best_index = contestant_indices[0]
-        for idx in contestant_indices:
-            if fitness_scores[idx] < fitness_scores[best_index]:
-                best_index = idx
         best_parent = population[best_index]
         return best_parent
 
+    # def crossover(self, parent1, parent2):
+    #     # Perform crossover between two parents to create a child
+    #     split_point = random.randint(1, min(len(parent1), len(parent2)) - 1)
+    #     child1 = parent1[:split_point] + parent2[split_point:]
+    #     child2 = parent2[:split_point] + parent1[split_point:]
+    #     return child1, child2
+
     def crossover(self, parent1, parent2):
-        # Perform crossover between two parents to create a child
-        split_point = random.randint(1, min(len(parent1), len(parent2)) - 1)
-        child1 = parent1[:split_point] + parent2[split_point:]
-        child2 = parent2[:split_point] + parent1[split_point:]
-        return child1, child2
+        k1 = self.find_k_point(parent1)
+        k2 = self.find_k_point(parent2)
+
+        child = None
+        if k1 == 0 and k2 == 0:
+            k = random.randint(1, min(len(parent1), len(parent2)) - 1)
+            child = parent1[:k] + parent2[k:]
+        elif k1 == k2 > 0:
+            child = parent1[:k1] + parent2[k2:]
+        elif k1 > k2:
+            if k2 == 0:
+                k = random.randint(1, len(parent2))
+                child = parent1[:k1] + parent2[k:]
+            else:
+                child = parent1[:k1] + parent2[k2:]
+        elif k1 < k2:
+            if k1 == 0:
+                k = random.randint(1, len(parent1))
+                child = parent2[:k2] + parent1[k:]
+            else:
+                child = parent2[:k2] + parent1[k1:]
+
+        return child
+
+    def find_k_point(self, chromosome):
+        x, y = self.src
+        k = 0
+        for gene in chromosome:
+            x, y = x + (gene == 'D') - (gene == 'U'), y + (gene == 'R') - (gene == 'L')
+            if x < 0 or x >= self.maze.shape[0] or y < 0 or y >= self.maze.shape[1] or self.maze[x][y] == 1:
+                return k
+            k += 1
+        return k
 
     def mutate(self, chromosome):
         new_chromosome = ''
-        for gene in chromosome:
-            if random.random() < self.mutation_rate:
-                new_chromosome += random.choice(['U', 'D', 'L', 'R'])
+        for i in range(len(chromosome)-1):
+            gene = chromosome[i]
+            next_gene = chromosome[i+1]
+            if (gene == 'L' and next_gene == 'R') or (gene == 'R' and next_gene == 'L') or \
+                (gene == 'U' and next_gene == 'D') or (gene == 'D' and next_gene == 'U'):
+                if random.random() < self.mutation_rate:
+                    possible_gene = random.choice(['U', 'D', 'L', 'R'])
+                new_chromosome +=
             else:
                 new_chromosome += gene
-        return new_chromosome
+        return chromosome
